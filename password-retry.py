07ae6af140dc120b06db3b2e6904512b3524f66e
Kevin Chiu <kc54170@gmail.com>
2019-10-12 20:20:18 +0800

    change to while i > 0

--- password-retry.py
+++ password-retry.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 x = 3
-while True:
+while i > 0:
 	user = input('請輸入密碼： ')
 	if user == password:
 		print('登入成功!')
@@ -8,5 +8,3 @@ while True:
 	else:
 		x = x - 1
 		print('密碼錯誤!還有', x ,'次機會')
-		if x == 0:
-			break
